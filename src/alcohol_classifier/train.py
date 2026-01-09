aa2c3c8cf30c2647d99cfa30812a012c3135f518
Leon Spohn <s242845@dtu.dk>
2026-01-09 12:25:23 +0100

    initial commit with new project name

---
+++ src/alcohol_classifier/train.py
@@ -0,0 +1,10 @@
+from alcohol_classifier.model import Model
+from alcohol_classifier.data import MyDataset
+
+def train():
+    dataset = MyDataset("data/raw")
+    model = Model()
+    # add rest of your training code here
+
+if __name__ == "__main__":
+    train()
